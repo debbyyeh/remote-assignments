2276a957c346e523063af2446bdf225d2de5e810
debby <app2984@gmail.com>
2022-07-01 19:53:11 +0800

    update assignment3

--- node.js
+++ node.js
@@ -1,40 +1,33 @@
 const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-//enter http://localhost:3000/
-// http
-//   .createServer(function (request, response) {
-//     response.writeHead(200, { 'Content-Type': 'text/plain' })
-//     response.write('Hello, My Server!')
-//     response.end()
-//   })
-//   .listen(3000)
 
-// app.use((req, res, next) => {
-//   console.log('someone is coming')
-//   next()
-// })
 // Assignment 1: Your First Web Server
 app.get('/', (req, res) => {
   res.send('Hello, My Server!')
 })
 
 // Assignment 2: Build Backend API for Front-End
-app.get('/getData', (req, res) => {
+
+var webRequest = (req, res, next) => {
   let number = req.query.number
-  let _url = req.url
-  let total = 0
-  let n = number
-  if (_url == '/getData' && number === 'xyz') {
-    res.send('Wrong Parameter')
-  } else if (_url == '/getData' && number > 0) {
+  let _url = req._url
+  if (_url == '/getData') {
+    next()
+  } else if (_url !== '/getData' && number > 0) {
+    let total = 0
     for (let i = 0; i <= number; i++) {
       total += i
     }
     res.send(`You get ${total}`)
   } else {
-    res.send('Lack of Parameter')
+    res.send('Wrong Parameter')
   }
+}
+
+app.get('/getData', webRequest, (req, res) => {
+  res.send('Lack of Parameter')
 })
 
 //pug
@@ -44,6 +37,16 @@ app.use(express.static('./public'))
 app.get('/', (req, res) => {
   res.render('sum.html')
 })
+//傳送資料到後端
+app.post('/getData', (req, res) => {
+  console.log(req.body.number)
+  //轉址
+  res.redirect('/public/sum.html')
+})
 
-let port = process.env.PORT || 3000
+//增加body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var port = process.env.PORT || 3000
 app.listen(port)
